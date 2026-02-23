1f0dbbfe2d6dc0bb8ebd5b6bf11f18ee32d4d72c
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-02-23 20:22:54 +0400

    Added missing routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 // Protect all other routes after this middleware
 router.use(authController.protect);
