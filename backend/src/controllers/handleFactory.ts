de6c15d55497132acfff99fe6f6be6a652308074
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-03-15 21:09:20 +0400

    Started Wokring on factroy functions

---
+++ backend/src/controllers/handleFactory.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import catchAsync from "./../utils/catchAsync.js";
+import prisma from "../prisma.js";
+
+type PrismaModel = keyof typeof prisma;
+
+export const getAll = (model: PrismaModel) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const delegate = prisma[model] as any;
+
+    const data = await delegate.findMany();
+
+    res.status(200).json({
+      status: "success",
+      results: data.length,
+      data: {
+        [model]: data,
+      },
+    });
+  });
