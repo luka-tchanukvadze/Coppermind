582204c508304e3d5442889f5400464e26d2305f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-03-16 18:50:29 +0400

    Working on BookRoutes

--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -5,6 +5,12 @@ import * as bookController from "../controllers/bookController.js";
 
 const router = express.Router();
 
-router.route("/").post(bookController.addBook).get(bookController.getAllBooks);
+// Protect all other routes after this middleware
+router.use(authController.protect);
+
+router
+  .route("/")
+  .post(bookController.addBook)
+  .get(authController.restrictTo("admin"), bookController.getAllBooks);
 
 export default router;
