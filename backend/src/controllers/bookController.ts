7f75a807ee1af5dce6489ea2fe6c93af0366b223
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-03-18 00:17:52 +0400

    adding custrom controllers

--- backend/src/controllers/bookController.ts
+++ backend/src/controllers/bookController.ts
@@ -2,7 +2,32 @@ import { Request, Response, NextFunction } from "express";
 import catchAsync from "../utils/catchAsync.js";
 import prisma from "../prisma.js";
 import AppError from "../utils/appError.js";
-import * as factory from "./handleFactory.js";
 
-export const addBook = factory.createOne("book");
-export const getAllBooks = factory.getAll("book");
+export const addBook = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = await prisma.book.create({
+      data: req.body,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        book: data,
+      },
+    });
+  },
+);
+
+export const getAllBooks = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = await prisma.book.findMany();
+
+    res.status(200).json({
+      status: "success",
+      results: data.length,
+      data: {
+        books: data,
+      },
+    });
+  },
+);
