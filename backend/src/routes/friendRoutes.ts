a6f57ef80e6494da28fdb4dd383547f66e25cd36
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-03-30 16:29:46 +0400

    TODO friends

--- backend/src/routes/friendRoutes.ts
+++ backend/src/routes/friendRoutes.ts
@@ -7,6 +7,15 @@ const router = express.Router();
 
 router.use(authController.protect);
 
+/*
+TODO:
+get       /friends                     // list my accepted friends
+get       /friends/requests            // list my pending incoming requests
+get       /friends/mutual/:userId      // mutual friends with someone
+post      /freinds/:userId             // send request
+patch     /friends/:id/accept          // accept request
+delete    /friends/:id                 // reject request OR remove friend (same action: delete the row)
+*/
 router.route("/");
 
 export default router;
